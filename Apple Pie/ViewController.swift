f91ef1956a960fe9044a92b1c0c1d0ea9a2b9a99
Gianluca Orpello <g.orpello@gmail.com>
2021-03-03 13:11:49 +0100

    first part of the exercise

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        print(sender.title(for: .normal)!)
+    }
+    
 
 }
 
